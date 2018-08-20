7bde71be2b6b37706cc8bc9bd18adc3be2e6235a
student <eco.vlada@gmail.com>
2018-08-20 19:10:09 +0300

    settings+begin of facebook

---
+++ RadioOnline/Tab Bar Controller/CollectionTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  CollectionTabBarController.swift
+//  RadioOnline
+//
+//  Created by student on 8/20/18.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class CollectionTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
