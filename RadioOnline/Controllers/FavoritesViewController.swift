0823a5f6493acb4ebf0a281f94e702b8995ae3ba
student <eco.vlada@gmail.com>
2018-08-22 10:13:45 +0300

    add custom picture to table view

--- RadioOnline/Controllers/FavoritesViewController.swift
+++ RadioOnline/Controllers/FavoritesViewController.swift
@@ -68,12 +68,29 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
         cell.descriptionLabel.text = DataManager.stationsFavorites[indexPath.row].desc
         cell.descriptionLabel.textColor = ContrastColorOf(tableView.backgroundColor!, returnFlat: true)
         
-        let img = DataManager.readImg(name: "\(DataManager.stationsFavorites[indexPath.row].name).png")
-        if img == nil || img == #imageLiteral(resourceName: "stationImage") {
-            cell.imageRadioStation.downloadedFrom(link: DataManager.stationsFavorites[indexPath.row].imageURL, name: "\(DataManager.stations[indexPath.row].name).png")
-        } else {
-            cell.imageRadioStation.image = img
-            //print("image load from device")
+        print(DataManager.stationsFavorites[indexPath.row].imageURL)
+        if DataManager.stationsFavorites[indexPath.row].imageURL.contains("user")
+        {
+            let fileManager = FileManager.default
+            let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let documentDirectory = URL(fileURLWithPath: path)
+            let destinationPath = documentDirectory.appendingPathComponent("\(DataManager.stationsFavorites[indexPath.row].name).jpg")
+            if fileManager.fileExists(atPath: destinationPath.path){
+                cell.imageRadioStation.image = UIImage(contentsOfFile: destinationPath.path)
+            }else{
+                print("No Image")
+                cell.imageRadioStation.image = #imageLiteral(resourceName: "stationImage")
+            }
+        }
+        else
+        {
+            let img = DataManager.readImg(name: "\(DataManager.stationsFavorites[indexPath.row].name).png")
+            if img == nil || img == #imageLiteral(resourceName: "stationImage") {
+                cell.imageRadioStation.downloadedFrom(link: DataManager.stationsFavorites[indexPath.row].imageURL, name: "\(DataManager.stations[indexPath.row].name).png")
+            } else {
+                cell.imageRadioStation.image = img
+                //print("image load from device")
+            }
         }
         
         if DataManager.stationsFavorites[indexPath.row].new == true {
