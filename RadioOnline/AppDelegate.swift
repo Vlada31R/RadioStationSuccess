77c71575ab9a58f847830bd30aa3af366c271e20
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2018-11-15 10:45:54 +0200

    test

--- RadioOnline/AppDelegate.swift
+++ RadioOnline/AppDelegate.swift
@@ -27,7 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        self.window?.rootViewController = initialViewController
 //        self.window?.makeKeyAndVisible()
         //make bandge
-
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let userDefaults = UserDefaults.standard
